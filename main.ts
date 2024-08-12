e33eac3c846552a8e74f7f25a52fe759320c83c9
鄢梦阳 <510857289@qq.com>
2024-08-12 16:04:23 +0800

    通信协议完善

--- main.ts
+++ main.ts
@@ -1,25 +1,26 @@
 
-
 /**
 * Functions to BluetoothInteraction by ELECFREAKS Co.,Ltd.
 */
 //% color=#00B1ED  icon="\uf005" block="BluetoothInteraction" blockId="BluetoothInteraction"
 namespace BluetoothInteraction {
 
-    let bleInitFlag = 0;
-    let bleConnFalg = 0;
-    let bleMsgState = 0;
-    let bleMsgCode = 0;
-    let bleMsgLength = 0;
-    let bleMsgBuf = [];
-    let bleMsgBufIndex = 0;
+    let bleInitFlag: number = 0;
+    let bleConnFalg: number = 0;
+    let bleMsgState: number = 0;
+    let bleHeaderBuf: number[] = [];
+    let bleHeaderBufIndex: number = 0;
+    let bleMsgBuf: number[] = [];
+    let bleMsgBufIndex: number = 0;
+    let bleCommandHandle: { [key: number]: (param: number[]) => number[] } = {};
 
     function handleBluetoothData(data: number) {
         switch (bleMsgState) {
             case 0:
                 if (data == 0xFF) {
-                    bleMsgState = 1;
                     bleMsgBufIndex = 0;
+                    bleHeaderBufIndex = 0;
+                    bleMsgState = 1;
                 }
                 break;
             case 1:
@@ -28,49 +29,89 @@ namespace BluetoothInteraction {
                 }
                 break;
             case 2:
-                bleMsgCode = data;
-                bleMsgState = 3;
-                break;
-            case 3:
-                bleMsgLength = data;
-                bleMsgState = 4;
-                break;
-            case 4:
-                bleMsgBuf[bleMsgBufIndex++] = data;
-                if (bleMsgLength == bleMsgBufIndex) {
-                    bleMsgState = 5;
+                bleHeaderBuf[bleHeaderBufIndex++] = data;
+                if (bleHeaderBufIndex == 5) {
+                    bleMsgState = 3;
                 }
                 break;
-            case 5:
-                let checksum = 0xFF + 0xF9 + bleMsgCode + bleMsgLength;
-                for (let i = 0; i < bleMsgLength; i++) {
+            case 3:
+                bleMsgBuf[bleMsgBufIndex++] = data;
+                if (bleHeaderBuf[4] == bleMsgBufIndex) {
+                    bleMsgState = 4;
+                }
+                break;
+            case 4:
+                let checksum = 0xFF + 0xF9;
+                for (let i = 0; i < bleHeaderBufIndex; i++) {
+                    checksum += bleHeaderBuf[i];
+                }
+                for (let i = 0; i < bleMsgBufIndex; i++) {
                     checksum += bleMsgBuf[i];
                 }
                 // 指令校验通过，调用对应函数
-                if (data == checksum & 0xFF) {
-                    handleBleCommand(bleMsgCode, bleMsgBuf);
+                if (data == (checksum & 0xFF)) {
+                    let bleMsgId = bleHeaderBuf[0] << 8 | bleHeaderBuf[1];
+                    let bleMsgCode = bleHeaderBuf[2] << 8 | bleHeaderBuf[3];
+                    handleBleCommand(bleMsgId, bleMsgCode, bleMsgBuf);
                 }
+                bleMsgState = 0;
                 break;
             default:
                 break;
         }
     }
+    function sendBleResult(id: number, code: number, msg: number[]) {
+        let buf = pins.createBuffer(8 + msg.length);
+        buf[0] = 0xFF;
+        buf[1] = 0xF9;
+        buf[2] = (id >> 8) & 0xFF;
+        buf[3] = id & 0xFF;
+        buf[4] = (code >> 8) & 0xFF;
+        buf[5] = code & 0xFF;
+        buf[6] = msg.length;
+        for (let i = 0; i < msg.length; i++) {
+            buf[7 + i] = msg[i];
+        }
+        let checksum = 0;
+        for (let i = 0; i < msg.length - 1; i++) {
+            checksum += msg[i];
+        }
+        buf[msg.length - 1] = checksum & 0xFF;
+        bluetooth.uartReadBuffer(buf); // 向蓝牙设备写数据
 
-    function handleBleCommand(code, msg) {
-        switch (code) {
-            case 0x1:
-                break;
-            default:
-                break;
+    }
+
+    function handleBleCommand(id: number, code: number, msg: number[]) {
+        if (bleCommandHandle[code] != undefined) {
+            let ret = bleCommandHandle[code](msg);
+            if (ret != undefined) {
+                sendBleResult(id, code, ret);
+            }
         }
     }
 
     /**
+     * 翻转LED灯
+     * @param msg[0] led x坐标
+     * @param msg[1] led y坐标
+     * @returns 返回1
+     */
+    function ledToggle(msg: number[]): number[] {
+        led.toggle(msg[0], msg[1]);
+        return [1];
+    }
+
+
+    
+
+    /**
      * 初始化蓝牙模块
      */
+    //% weight=99
+    //% block="BluetoothInteraction Init"
     export function bluetoothServerInit() {
         if (bleInitFlag == 0) {
-
+            basic.pause(100)
             bluetooth.startUartService()
             bluetooth.setTransmitPower(7)
             bluetooth.onBluetoothConnected(function () {
@@ -79,8 +120,7 @@ namespace BluetoothInteraction {
             bluetooth.onBluetoothDisconnected(function () {
                 bleConnFalg = 0;
             })
-
-            control.inBackground(function () {
+            basic.forever(function () {
                 if (bleConnFalg == 1) {
                     let buf = bluetooth.uartReadBuffer();
                     if (buf.length == 0) {
@@ -92,9 +132,9 @@ namespace BluetoothInteraction {
                 }
             })
 
+            bleCommandHandle[0x01] = ledToggle;
+
             bleInitFlag = 1;
         }
     }
-
-
 }

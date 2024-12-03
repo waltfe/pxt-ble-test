8f216aa86d7b4b88f415dcc8c568e60b9d0b4384
zhuyu <zhuyu@elecfreaks.com>
2024-12-03 19:58:37 +0800

    ashdi

--- main.ts
+++ main.ts
@@ -48,6 +48,7 @@ namespace BluetoothInteraction {
                     bleMsgBufIndex = 0;
                     bleHeaderBufIndex = 0;
                     bleMsgState = 1;
+                    bleMsgBuf.length = 0;
                 }
                 break;
             case 1:
